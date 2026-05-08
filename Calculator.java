31a8a9aa043075f890840d816bd5ac5b1bb36ecf
TasKid32 <203917777+TasKid32@users.noreply.github.com>
2026-05-09 01:10:16 +0300

    Added Subtraction feature

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,11 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Calculator!");
-        
-        System.out.println("Feature: Addition added!");
-        System.out.println("2 + 3 = " + add(2, 3));
+        System.out.println("Feature: Subtraction added!");
+        System.out.println("5 - 2 = " + subtract(5, 2));
     }
 
-    public static int add(int a, int b) {
-        return a + b;
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
