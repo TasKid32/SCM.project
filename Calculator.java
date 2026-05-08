9753178341e6b5f217dd110da9f5f38f967861f0
TasKid32 <203917777+TasKid32@users.noreply.github.com>
2026-05-09 01:04:02 +0300

    Added addition feature

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,12 @@
 public class Calculator {
-
     public static void main(String[] args) {
         System.out.println("Welcome to Java Calculator!");
-        System.out.println("Version 1.0");
+        
+        System.out.println("Feature: Addition added!");
+        System.out.println("2 + 3 = " + add(2, 3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
