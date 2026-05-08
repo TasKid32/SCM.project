0ffddd36b7109d48d059ee0efb0a9a53ef57d98e
TasKid32 <203917777+TasKid32@users.noreply.github.com>
2026-05-09 00:57:59 +0300

    Initial commit - v1.0

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Calculator!");
+        System.out.println("Version 1.0");
+    }
+}
